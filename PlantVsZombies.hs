153369fccdb07ce149099360e6a5a49f32f62689
Manu <manumc7910@gmail.com>
2019-04-09 19:46:01 -0300

    Agregue si una linea esta en peligro

--- PlantVsZombies.hs
+++ PlantVsZombies.hs
@@ -1,49 +1,26 @@
 --1a)
-
 data Planta = UnaPlanta {
-
  ptosDevida :: Int,
-
  solesProducidos :: Int, 
-
  poderDeAtaque :: Int
-
 } deriving (Show)
 
-
-
 peaShooter = UnaPlanta {
-
  ptosDevida = 5,
-
  solesProducidos = 0,
-
  poderDeAtaque = 2
-
 }
-
-
 
 repeater = UnaPlanta {
-
  ptosDevida = 5,
-
  solesProducidos = 0,
-
  poderDeAtaque = (*2).poderDeAtaque $ peaShooter
-
 }
 
-
-
 sunflower = UnaPlanta {
-
  ptosDevida = 7,
-
  solesProducidos = 1,
-
  poderDeAtaque = 0
-
 } 
 
 nut = UnaPlanta {
@@ -53,159 +30,100 @@ nut = UnaPlanta {
 }
 
 --1b) 
-
 data Zombie = UnZombie {
-
  nombre :: String,
-
  accesorios :: [String],
-
  danioPorMordida :: Int,
-
  nivelDeMuerte :: Int
-
 } deriving (Show)
 
-
-
 zombieBase = UnZombie {
-
  nombre = "Zombie",
-
  accesorios = [],
-
  danioPorMordida = 1,
-
  nivelDeMuerte = saberNivelDeMuerte zombieBase 
-
 }
-
-
 
 balloonZombie = UnZombie {
-
  nombre = "Pepe Colgado",
-
  accesorios = ["Globo"],
-
  danioPorMordida = 1,
-
  nivelDeMuerte = saberNivelDeMuerte balloonZombie
-
 }
-
-
 
 newspaperZombie = UnZombie {
-
  nombre = "Beto el chismoso",
-
  accesorios = ["Diario"],
-
  danioPorMordida = 2,
-
  nivelDeMuerte = saberNivelDeMuerte newspaperZombie
-
 }
-
-
 
 gargantuar = UnZombie {
-
  nombre = "Gargantuar Hulk Smash Puny God",
-
  accesorios = ["Poste","Enano"],
-
  danioPorMordida = 30,
-
  nivelDeMuerte = saberNivelDeMuerte gargantuar
-
 }
 
-
-
 saberNivelDeMuerte :: Zombie -> Int
-
 saberNivelDeMuerte = length.nombre
 
-
-
 --2a)
-
 especialidad :: Planta -> String
-
 especialidad planta
-
  | (solesProducidos planta) > 0 = "Proveedora"
-
  | ((*2).poderDeAtaque $ planta) > (ptosDevida planta) = "Atacante" 
-
  | otherwise = "Defensiva"
 
 --2b)
-
 esPeligroso :: Zombie -> Bool
-
 esPeligroso zombie = (length.accesorios $ zombie) > 1 || (nivelDeMuerte zombie) > 10
 
 --3a)
-
 data LineaDeDefensa = UnaLineaDeDefensa {
-
  plantas :: [Planta],
-
  zombies :: [Zombie]
-
 } deriving (Show)
 
-
-
 linea1 = UnaLineaDeDefensa {
-
  plantas = [sunflower, sunflower, sunflower],
-
  zombies = []
-
 }
 
 linea2 = UnaLineaDeDefensa {
-
  plantas = [peaShooter, peaShooter, sunflower, nut],
-
  zombies = [zombieBase, newspaperZombie]
-
 }
 
 linea3 = UnaLineaDeDefensa {
-
  plantas = [sunflower, peaShooter],
-
  zombies = [gargantuar, zombieBase, zombieBase]
-
 }
 
 linea4 = UnaLineaDeDefensa {
-
  plantas = [peaShooter],
-
  zombies = [zombieBase]
-
 }
 
-
-
 agregarPlanta :: LineaDeDefensa -> Planta -> LineaDeDefensa
-
 agregarPlanta linea planta = linea{plantas = agregarAfila (plantas linea) planta}
 
-
-
 agregarZombie :: LineaDeDefensa -> Zombie -> LineaDeDefensa
-
 agregarZombie linea zombie = linea{zombies = agregarAfila (zombies linea) zombie}
 
-
-
 agregarAfila :: [a] -> a -> [a]
-
 agregarAfila linea plantaOzombie = linea++[plantaOzombie]
+
+--3b)
+
+estaEnPeligro :: LineaDeDefensa -> Bool
+estaEnPeligro linea = (poderDeAtaqueTotal linea) < (danioPorMordidaTotal linea) || sonTodosPeligrosos linea
+
+poderDeAtaqueTotal :: LineaDeDefensa -> Int
+poderDeAtaqueTotal linea = sum.(map poderDeAtaque) $ (plantas linea)
+
+danioPorMordidaTotal :: LineaDeDefensa -> Int
+danioPorMordidaTotal linea = sum.(map danioPorMordida) $ (zombies linea)
+
+sonTodosPeligrosos :: LineaDeDefensa -> Bool
+sonTodosPeligrosos linea = all esPeligroso (zombies linea) 
