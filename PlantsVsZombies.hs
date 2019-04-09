4faeeed1d959ad68fe02b31654f3f902d8d769d0
Manu <manumc7910@gmail.com>
2019-04-09 09:05:06 -0300

    Delete especie
    Por ahora no se requiere del atributo "especie"

--- PlantsVsZombies.hs
+++ PlantsVsZombies.hs
@@ -1,26 +1,22 @@
 data Planta = UnaPlanta {
- especie :: String,
  ptosDevida :: Int,
  solesProducidos :: Int, 
  poderDeAtaque :: Int
 }
 
 peaShooter = UnaPlanta {
- especie = especialidad peaShooter,
  ptosDevida = 5,
  solesProducidos = 0,
  poderDeAtaque = 2
 }
 
 repeater = UnaPlanta {
- especie = especialidad repeater,
  ptosDevida = 5,
  solesProducidos = 0,
  poderDeAtaque = (*2).poderDeAtaque $ peaShooter
 }
 
 sunflower = UnaPlanta {
- especie = especialidad sunflower,
  ptosDevida = 7,
  solesProducidos = 1,
  poderDeAtaque = 0
